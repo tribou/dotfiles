9139ceef2538fcfe0c437d1eb37a69d0d21b5e25
Aaron Tribou <aaron.tribou@gmail.com>
2016-03-03 11:19:23 -0600

    Put reminder text in center of terminal

--- scripts/remind.sh
+++ scripts/remind.sh
@@ -18,13 +18,26 @@ Example:
   fi
 
   SCREEN_LINES=`tput lines`
+  SCREEN_COLUMNS=`tput cols`
+  MESSAGE_LENGTH=${#1}
   MESSAGE_LINE=$(( $SCREEN_LINES / 2 ))
+  SCREEN_COLUMNS_HALF=$(( $SCREEN_COLUMNS / 2 ))
+  MESSAGE_LENGTH_HALF=$(( $MESSAGE_LENGTH / 2 ))
+  MESSAGE_COLUMN=$(( $SCREEN_COLUMNS_HALF - $MESSAGE_LENGTH_HALF ))
+  echo "$MESSAGE_COLUMN"
 
   for i in `seq 1 "$SCREEN_LINES"`
   do
     if [[ i -eq "$MESSAGE_LINE" ]]
     then
+
+      for i in `seq 1 "$MESSAGE_COLUMN"`
+      do
+        printf " "
+      done
+
       echo "$1"
+
     else
       echo
     fi
