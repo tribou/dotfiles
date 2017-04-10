fda7d1eec764c0533fe22b79a81db1aee165faf2
Aaron Tribou <aaron.tribou@gmail.com>
2017-04-10 11:37:58 -0500

    Update curlit to accept seconds interval

--- scripts/curl_it.sh
+++ scripts/curl_it.sh
@@ -4,20 +4,42 @@
 
 curlit() {
 
-  usage='Usage: curlit URL'
-  search_dir='.'
+  usage='Usage: curlit URL [interval_seconds]
+
+  Example: curlit https://www.google.com/ 5
+  '
 
   # Return usage if 0 or more than 1 args are passed
-  if [ $# -eq 0 ] || [ $# -gt 1 ]
+  if [ $# -eq 0 ] || [ $# -gt 2 ]
   then
     echo "$usage"
     return 1
+  fi
+
+  if [ "$1" = "help" ]
+  then
+    echo "$usage"
+    return 0
+  fi
+
+  if [ "$2" -gt 0 ]
+  then
+    interval="$2"
+  else
+    interval=1
   fi
 
   while true; do
     RESULT_OUTPUT=$(curl -Is $1 | head -n1)
     printf $(date +%Y-%m-%dT%H:%M:%S%z)
     echo " $RESULT_OUTPUT"
-    sleep 1
+
+    if [ "$?" -ne 0 ]
+    then
+      echo "Call to $1 failed"
+      return 1
+    fi
+
+    sleep $interval
   done
 }
