4f76e4b8b594021507fa1feddc296e629bc48df6
Aaron Tribou <aaron.tribou@gmail.com>
2017-09-04 11:49:24 -0500

    Fix bfg command

--- lib/commands.sh
+++ lib/commands.sh
@@ -5,7 +5,7 @@ alias amend='git commit -S --amend'
 alias b='git branch -a'
 alias back='cd "$OLDPWD"'
 alias bd='docker-machine'
-alias bfg='java -jar bfg.jar'
+alias bfg='java -jar /usr/local/bin/bfg.jar'
 alias c='git commit -S -ev'
 alias cherry='git cherry-pick -S -x'
 function clean () {
