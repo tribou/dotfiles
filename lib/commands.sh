df84d24bb0a461559948e051941efa2b1c9910c2
Aaron Tribou <aaron.tribou@getbuilt.com>
2019-01-09 16:08:46 -0600

    Revert the histgrep sort order

--- lib/commands.sh
+++ lib/commands.sh
@@ -128,7 +128,7 @@ function gr ()
 alias gr2='git rebase -S -i head~2'
 function histgrep ()
 {
-  { (grep -r "$1" ~/.history | sort -r); (history | grep "$1") }
+  { (grep -r "$1" ~/.history | sort); (history | grep "$1") }
 }
 function install-swap ()
 {
