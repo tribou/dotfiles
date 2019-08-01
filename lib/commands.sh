4a829afe85c96e3cac4bb59a9179148e30beb843
Aaron Tribou <aaron.tribou@getbuilt.com>
2019-08-01 12:31:33 -0500

    Rewrite c commit helper function Reorganized aliases and functions

--- lib/commands.sh
+++ lib/commands.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -l
 
+# Helper functions
+function _dotfiles_git_status () {
+  git status -sb
+}
+
+function _dotfiles_grep_ticket_number () {
+  grep -E '^[A-Z0-9]{2,4}-\d{1,7}$'
+}
+
 # Use like: useLocalIfAvailable flow-typed -v
 function useLocalIfAvailable ()
 {
@@ -19,58 +28,44 @@ function useLocalIfAvailable ()
   fi
 }
 
-# Commands and aliases
-alias ack='ag'
-alias ag='rg'
-alias amend='git commit -S --amend'
-alias b='git branch -a --sort=-committerdate'
-alias back='cd "$OLDPWD"'
-alias bd='docker-machine'
-alias be='bundle exec'
-alias bfg='java -jar /usr/local/bin/bfg.jar'
-
+# If no args are passed, open the commit editor. Otherwise commit with all
+# arguments concatenated as a string
 function c ()
 {
-  if [ -f "$(which ./node_modules/.bin/git-cz)" ]
+  if [ $# -eq 0 ]
   then
-    # Use local commitizen if available
-    ./node_modules/.bin/git-cz -S
-  elif [ -f "$(which git-cz)" ]
-  then
-    # Then check for global commitizen
-    git-cz -S
+    git commit -S -ev && _dotfiles_git_status
   else
-    # Otherwise, use normal git commit
-    git commit -S -ev
+    local current_ticket=$(git branch --show-current | _dotfiles_grep_ticket_number)
+    if [ -z "$current_ticket" ]
+    then
+      local message="$*"
+    else
+      local message="$current_ticket: $*"
+    fi
+    git commit -S -m "$message" && _dotfiles_git_status
   fi
 }
-alias cherry='git cherry-pick -S -x'
-function clean () {
 
+function clean ()
+{
   git clean -f -- build/ public/ vendor/
   if [[ $? -ne 0 ]]; then return 1; fi
   if [ -d "build/" ]; then git checkout build/; fi
   if [ -d "public/" ]; then git checkout public/; fi
   if [ -d "vendor/" ]; then git checkout vendor/; fi
+}
 
-}
-function co()
+function co ()
 {
-  git checkout $@ && git status -sb
+  git checkout "$@" && _dotfiles_git_status
 }
-alias commit='git commit -ev' # non-signed commit
-alias convert-crlf-lf='git ls-files -z | xargs -0 dos2unix'
-alias convert-tabs-spaces="replace '	' '  '"
-alias count='sed "/^\s*$/d" | wc -l | xargs'
-alias d='docker'
-alias dc='docker-compose'
-alias di='docker images'
+
 function digitalocean ()
 {
   curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_API_TOKEN" "https://api.digitalocean.com/v2/$1?page=1&per_page=1000" | python -m json.tool
 }
-alias dm='docker-machine'
-#alias dminit='eval "$(docker-machine env $(docker-machine ls --filter driver=virtualbox --filter state=Running --format "{{.Name}}"))"'
+
 function dminit ()
 {
   local usage='Usage: dminit [NAME]'
@@ -90,11 +85,8 @@ function dminit ()
 
   eval "$(docker-machine env $dm_name)"
 }
-alias dps='docker ps'
-alias dpsa='docker ps -a'
-alias drm='docker rm'
-alias drmi='docker rmi'
-function da()
+
+function da ()
 {
   # Select a docker container to start and attach to
   local cid
@@ -102,7 +94,7 @@ function da()
 
   [ -n "$cid" ] && docker start "$cid" && docker attach "$cid"
 }
-function ds()
+function ds ()
 {
   # Select a running docker container to stop
   local cid
@@ -110,31 +102,17 @@ function ds()
 
   [ -n "$cid" ] && docker stop "$cid"
 }
-alias edld='ember deploy:list --environment development'
-alias edlp='ember deploy:list --environment production'
-alias edls='ember deploy:list --environment staging'
-function f()
+
+function f ()
 {
-  git fetch --prune --progress $@ && git status -sb
+  git fetch --prune --progress "$@" && _dotfiles_git_status
 }
-alias filetypes="git ls-files | sed 's/.*\.//' | sort | uniq -c"
-alias fix='git commit --amend -a --no-edit -S'
-function ga()
+
+function ga ()
 {
-  git add --all $@ && git status -sb
+  git add --all "$@" && _dotfiles_git_status
 }
-alias gall='echo; echo; git log --oneline --all --graph --decorate  $(git reflog | awk '"'"'{print $1}'"'"')'
-alias gc='gcloud compute'
-alias gci='gcloud compute instances'
-alias gbd='git branch -d'
-alias gbdr='git branch -d -r'
-alias gd='git diff'
-alias gds='git diff --staged'
-alias gf='git flow'
-alias gfix='git commit --amend -a --no-edit -S'
-alias gps='git push'
-alias gpst='git push --follow-tags'
-alias gpl='git pull'
+
 function gr ()
 {
 
@@ -149,15 +127,12 @@ function gr ()
 
   if [ $# -gt 1 ]
   then
-    git rebase -S $@
+    git rebase -S "$@"
   else
     git rebase -S -i HEAD~$1
   fi
-
 }
-alias gr2='git rebase -S -i HEAD~2'
-alias gs='git show'
-alias hg='histgrep'
+
 function histgrep ()
 {
   # Remove histfile directory prefix during fzf search
@@ -180,9 +155,9 @@ function histgrep ()
   echo "$RESULT"
   printf "$RESULT" | pbcopy
 }
+
 function install-swap ()
 {
-
   local usage='Usage: install-swap HOST'
 
   # Return usage if 0 or more than 2 args are passed
@@ -200,19 +175,10 @@ function install-swap ()
   ansible-playbook $DEVPATH/ansible-swap/site.yml \
     -i "${1}," \
     --extra-vars "target=${1}"
-
 }
-alias k='kubectl'
-alias kg='kubectl get pods,rc,svc,ing -o wide --show-labels'
-alias kd='kubectl describe'
-alias less='less -r'
-alias ll='ls -lah'
-alias ls='ls -G'
-alias lt='ls -lath'
-alias merge='git merge -S'
+
 function new-docker ()
 {
-
   local usage='Usage: new-docker [NAME] [ACCESS_TOKEN]'
 
   if [ $# -gt 3 ]
@@ -247,11 +213,10 @@ function new-docker ()
   local MACHINE_IP=$(docker-machine ip "$MACHINE_NAME") && \
   install-swap "${MACHINE_IP}" && \
   dminit "${MACHINE_NAME}"
-
 }
+
 function new-docker-generic ()
 {
-
   local usage='Usage: new-docker-generic IP_ADDRESS [NAME] [PRIVATE_KEY_PATH]'
 
   if [ $# -lt 1 ]
@@ -282,11 +247,8 @@ function new-docker-generic ()
     "${MACHINE_NAME}" && \
   install-swap "${MACHINE_IP}" && \
   dminit "${MACHINE_NAME}"
-
 }
-alias ni='npm install'
-alias nis='npm install --save'
-alias nisd='npm install --save-dev'
+
 function npm-install-global ()
 {
   if [ ! -z "$1" ]
@@ -316,6 +278,129 @@ function npm-install-global ()
     vscode-css-languageserver-bin \
     vscode-html-languageserver-bin
 }
+
+function search ()
+{
+
+  local usage='Usage: search PATTERN'
+
+  # Return usage if 0 or more than 2 args are passed
+  if [ $# -ne 1 ]
+  then
+    echo "$usage"
+    return 1
+  fi
+
+  echo
+  echo
+  git grep -n -I --untracked --break "$1" -- './*' \
+    ':!bin/**' \
+    ':!flow-typed/**' \
+    ':!vendor/**' \
+    ':!yarn.lock'
+}
+
+# function to execute built-in cd
+function z_cd ()
+{
+  if [ $# -le 1 ]; then
+    fasd "$@"
+  else
+    local _fasd_ret="$(fasd -e 'printf %s' "$@")"
+    [ -z "$_fasd_ret" ] && return
+    [ -d "$_fasd_ret" ] && cd "$_fasd_ret" || printf %s\n "$_fasd_ret"
+  fi
+}
+
+function zz ()
+{
+  local dir
+  dir="$(fasd -Rdl "$1" | fzf -1 -0 --no-sort +m)" && cd "${dir}" || return 1
+}
+
+# Command aliases
+alias ack='ag'
+alias ag='rg'
+alias amend='git commit -S --amend'
+alias b='git branch -a --sort=-committerdate'
+alias back='cd "$OLDPWD"'
+alias bd='docker-machine'
+alias be='bundle exec'
+alias bfg='java -jar /usr/local/bin/bfg.jar'
+alias cherry='git cherry-pick -S -x'
+alias commit='git commit -ev' # non-signed commit
+alias convert-crlf-lf='git ls-files -z | xargs -0 dos2unix'
+alias convert-tabs-spaces="replace '	' '  '"
+alias count='sed "/^\s*$/d" | wc -l | xargs'
+alias d='docker'
+alias dc='docker-compose'
+alias di='docker images'
+alias dm='docker-machine'
+#alias dminit='eval "$(docker-machine env $(docker-machine ls --filter driver=virtualbox --filter state=Running --format "{{.Name}}"))"'
+alias dps='docker ps'
+alias dpsa='docker ps -a'
+alias drm='docker rm'
+alias drmi='docker rmi'
+alias edld='ember deploy:list --environment development'
+alias edlp='ember deploy:list --environment production'
+alias edls='ember deploy:list --environment staging'
+alias filetypes="git ls-files | sed 's/.*\.//' | sort | uniq -c"
+alias fix='git commit --amend -a --no-edit -S'
+alias gall='echo; echo; git log --oneline --all --graph --decorate  $(git reflog | awk '"'"'{print $1}'"'"')'
+alias gc='gcloud compute'
+alias gci='gcloud compute instances'
+alias gbd='git branch -d'
+alias gbdr='git branch -d -r'
+alias gd='git diff'
+alias gds='git diff --staged'
+alias gf='git flow'
+alias gfix='git commit --amend -a --no-edit -S'
+alias gps='git push'
+alias gpst='git push --follow-tags'
+alias gpl='git pull'
+alias gr2='git rebase -S -i HEAD~2'
+alias gs='git show'
+alias hg='histgrep'
+alias k='kubectl'
+alias kg='kubectl get pods,rc,svc,ing -o wide --show-labels'
+alias kd='kubectl describe'
+alias less='less -r'
+alias ll='ls -lah'
+alias ls='ls -G'
+alias lt='ls -lath'
+alias merge='git merge -S'
+alias ni='npm install'
+alias nis='npm install --save'
+alias nisd='npm install --save-dev'
+alias nr='npm run --silent'
+alias nrs='npm rm --save'
+alias nrsd='npm rm --save-dev'
+alias prettyjson='python -m json.tool'
+alias proxy-mini='ssh -D 8001 tbomini-remote'
+alias r='git remote -v'
+alias remote-mini='ssh -L 9000:localhost:5900 -L 35729:localhost:35729 -L 4200:localhost:4200 -L 3000:localhost:3000 -L 8090:localhost:8090 -L 8000:localhost:8000 tbomini-remote'
+alias revert='git revert -S HEAD'
+alias s='_dotfiles_git_status'
+alias setdotglob='shopt -s dotglob'
+alias sprofile='. ~/.bash_profile; cd "$OLDPWD"'
+alias survey='sudo nmap -sP 10.0.1.1/24'
+alias t='echo; echo; git tree'
+alias ts='echo; echo; git tree-short'
+alias tag='git tag -s -m ""'
+alias top='top -o cpu'
+alias tree='tree -I "bower_components|dist|node_modules|temp|tmp"'
+alias unsetdotglob='shopt -u dotglob'
+alias v='nvim'
+alias vc='vimcat'
+alias vim='nvim'
+alias webpack='useLocalIfAvailable webpack'
+alias webpack-bundle-analyzer='npx webpack-bundle-analyzer'
+alias y='npm run --silent yarn-bin --'
+alias yi='npm run --silent yarn-bin --'
+alias yr='npm run --silent yarn-bin --'
+alias youcompleteme-install='cd ~/.vim/plugged/YouCompleteMe; ./install.py --clang-completer --gocode-completer --tern-completer; cd "$OLDPWD"'
+alias z='z_cd -d'
+
 # NPM GLOBAL ALIASES
 # Instead of installing ALL CLI packages globally, we can use NPX to call the
 # ones we need in bash
@@ -337,69 +422,3 @@ alias nsp='npx nsp'
 alias npm-check-updates='npx npm-check-updates'
 alias storybook='npx @storybook/cli'
 alias ember='npx ember-cli'
-
-
-alias nr='npm run --silent'
-alias nrs='npm rm --save'
-alias nrsd='npm rm --save-dev'
-alias prettyjson='python -m json.tool'
-alias proxy-mini='ssh -D 8001 tbomini-remote'
-alias r='git remote -v'
-alias remote-mini='ssh -L 9000:localhost:5900 -L 35729:localhost:35729 -L 4200:localhost:4200 -L 3000:localhost:3000 -L 8090:localhost:8090 -L 8000:localhost:8000 tbomini-remote'
-alias revert='git revert -S HEAD'
-alias s='git status -sb'
-function search ()
-{
-
-  local usage='Usage: search PATTERN'
-
-  # Return usage if 0 or more than 2 args are passed
-  if [ $# -ne 1 ]
-  then
-    echo "$usage"
-    return 1
-  fi
-
-  echo
-  echo
-  git grep -n -I --untracked --break "$1" -- './*' \
-    ':!bin/**' \
-    ':!flow-typed/**' \
-    ':!vendor/**' \
-    ':!yarn.lock'
-}
-alias setdotglob='shopt -s dotglob'
-alias sprofile='. ~/.bash_profile; cd "$OLDPWD"'
-alias survey='sudo nmap -sP 10.0.1.1/24'
-alias t='echo; echo; git tree'
-alias ts='echo; echo; git tree-short'
-alias tag='git tag -s -m ""'
-alias top='top -o cpu'
-alias tree='tree -I "bower_components|dist|node_modules|temp|tmp"'
-alias unsetdotglob='shopt -u dotglob'
-alias v='nvim'
-alias vc='vimcat'
-alias vim='nvim'
-alias webpack='useLocalIfAvailable webpack'
-alias webpack-bundle-analyzer='npx webpack-bundle-analyzer'
-alias y='npm run --silent yarn-bin --'
-alias yi='npm run --silent yarn-bin --'
-alias yr='npm run --silent yarn-bin --'
-alias youcompleteme-install='cd ~/.vim/plugged/YouCompleteMe; ./install.py --clang-completer --gocode-completer --tern-completer; cd "$OLDPWD"'
-# function to execute built-in cd
-function z_cd()
-{
-  if [ $# -le 1 ]; then
-    fasd "$@"
-  else
-    local _fasd_ret="$(fasd -e 'printf %s' "$@")"
-    [ -z "$_fasd_ret" ] && return
-    [ -d "$_fasd_ret" ] && cd "$_fasd_ret" || printf %s\n "$_fasd_ret"
-  fi
-}
-alias z='z_cd -d'
-function zz()
-{
-  local dir
-  dir="$(fasd -Rdl "$1" | fzf -1 -0 --no-sort +m)" && cd "${dir}" || return 1
-}
