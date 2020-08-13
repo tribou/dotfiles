a749b59c24e2e658f0ee7043f97fce1a02f21961
Aaron Tribou <tribou@users.noreply.github.com>
2020-08-13 13:51:25 -0500

    Update nvim install

--- scripts/install.sh
+++ scripts/install.sh
@@ -28,6 +28,6 @@ then
 
     curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
     chmod u+x nvim.appimage
-    ./nvim.appimage
+    sudo mv ./nvim.appimage /usr/bin/nvim
   fi
 fi
