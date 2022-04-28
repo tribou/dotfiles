737cfbca36e4ddce1817edee1e77945a70b5262d
Aaron Tribou <tribou@users.noreply.github.com>
2022-04-28 16:59:22 -0500

    Fix the battery script when <10%

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -4,7 +4,7 @@
 
 if [ -s "$(which pmset)" ]
 then
-  pmset -g batt | grep [0-9][0-9]% | awk '{print $3}' | cut -c 1-4 | sed -E 's/;//'
+  pmset -g batt | grep [0-9]% | awk '{print $3}' | cut -c 1-4 | sed -E 's/;//'
 else
   echo "N/A"
 fi
