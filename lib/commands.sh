1372bba5c3556c3d8ba1a3c709f9ad3fa64e170e
Aaron Tribou <tribou@users.noreply.github.com>
2024-09-01 22:12:26 -0500

    Echo AWS_ACCOUNT_ID after setting

--- lib/commands.sh
+++ lib/commands.sh
@@ -18,6 +18,7 @@ function aws-profile() {
 function aws-set-current-account-id () {
   AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
   export AWS_ACCOUNT_ID
+  echo "AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID"
 }
 
 # If no args are passed, open the commit editor. Otherwise commit with all
